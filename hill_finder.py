6d0c0269c6656ea1418f0499ade04a786cb2939c
Evan Bretl <evanbretl4@gmail.com>
2016-11-05 12:34:43 -0400

    test movement along graph

--- hill_finder.py
+++ hill_finder.py
@@ -1,5 +1,6 @@
-from subprocess import check_output
 import sys
+import random
+from subprocess import check_output
 from map_to_graph import map_to_graph
 from math import sin, cos, sqrt, atan2, radians
 import osmapi
@@ -26,7 +27,49 @@ except: # osmapi.OsmApi.MaximumRetryLimitReachedError:  #TODO: handle errors
 
 graph = map_to_graph(map_data)
 
-edge_len_max = 0
+# follow a random path
+test_path = list()
+curnode = random.choice(tuple(graph.keys()))
+test_path.append(curnode)
+for i in range(49):
+    options = graph[curnode] - set(test_path);
+    if len(options) == 0:
+        print('no links :(')
+        break
+    curnode = random.choice(tuple(options))
+    test_path.append(curnode)
+
+
+def get_node_entry_by_id(target_node):
+    return (item for item in map_data 
+            if item["data"]["id"] == target_node).__next__()
+
+def latlong_dist(lat1_raw, lon1_raw, lat2_raw, lon2_raw):
+    lat1 = radians(float(lat1_raw))
+    lon1 = radians(float(lon1_raw))
+    lat2 = radians(float(lat2_raw))
+    lon2 = radians(float(lon2_raw))
+    # approximate radius of earth in m
+    R = 6373000.0
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+    return distance
+
+with open('pins.txt', 'w') as trace_file:
+    for node in test_path:
+        node_info = get_node_entry_by_id(node)
+        lat = node_info['data']['lat']
+        lon = node_info['data']['lon']
+        write_string = str(lat) + ', ' + str(lon) + '\n'
+        trace_file.write(write_string)
+
+
+
+
+'''#edge_len_max = 0
 for entry in map_data:
     if (entry['type'] == 'node'
             and 'data' in entry.keys()
@@ -34,29 +77,16 @@ for entry in map_data:
             and entry['data']['id'] in graph.keys()
     ):
         for neighbor in graph[entry['data']['id']]:
-            neighbor_entry = (item for item in map_data 
-                              if item["data"]["id"] == neighbor).__next__()
+            neighbor_entry = get_node_entry_by_id(neighbor)
             
-            # approximate radius of earth in m
-            R = 6373000.0
+            # distance = latlong_dist(
+            #         entry['data']['lat'],
+            #         entry['data']['lon'],
+            #         neighbor_entry['data']['lat'],
+            #         neighbor_entry['data']['lon']
+            # )
+            # print('Result:', distance)
+            # edge_len_max = max(edge_len_max, distance)
 
-            lat1 = radians(float(entry['data']['lat']))
-            lon1 = radians(float(entry['data']['lon']))
-            lat2 = radians(float(neighbor_entry['data']['lat']))
-            lon2 = radians(float(neighbor_entry['data']['lon']))
-
-            #print(lat1, lon1, lat2, lon2)
-
-            dlon = lon2 - lon1
-            dlat = lat2 - lat1
-
-            a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-            c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-            distance = R * c
-            print("Result:", distance)
-
-            edge_len_max = max(edge_len_max, distance)
-
-print('\n', 'Maximum:', edge_len_max)
-
+#print('\n', 'Maximum:', edge_len_max)
+'''
