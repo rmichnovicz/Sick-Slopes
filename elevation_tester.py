f2717d1b3bf41c00a19e914a0a4fc4c187701c80
Evan Bretl <evanbretl4@gmail.com>
2016-11-05 15:04:03 -0400

    identifies local maxima within the set of osm nodes

---
+++ elevation_tester.py
@@ -0,0 +1,6 @@
+import hill_finder
+
+lons = [-83.5, -83.5, -84.5]
+lats = [33.5, 34.5, 33.5]
+
+print(hill_finder.get_elevations_by_coords(lats, lons))
--- hill_finder.py
+++ hill_finder.py
@@ -1,92 +1,134 @@
 import sys
 import random
-from subprocess import check_output
+import subprocess
 from map_to_graph import map_to_graph
-from math import sin, cos, sqrt, atan2, radians
+import math
 import osmapi
 
-def get_elevation(lat, lng):
-    database_path = 'grdn' + str(math.ceil(abs(lat))) + 'w' \
-         + str(math.ceil(abs(lng))).zfill(3) + '_13/w001001.adf'
-    #print(database_path)
-    elevation = float(check_output(
-        ['gdallocationinfo',database_path,
-         '-valonly','-geoloc',str(lng),str(lat)
-         ])
-    )
-    return elevation
+def get_elevations_by_coords(lats, lngs):
+    queries = dict()
+    for lat, lng in zip(lats, lngs):
+        fname = ('grd' + ('n' if lat>0 else 's') 
+                 + str(math.ceil(abs(lat))).zfill(2)
+                 + ('e' if lng>0 else 'w')
+                 + str(math.ceil(abs(lng))).zfill(3)
+                 )
 
+        s = str(lng) + ' ' + str(lat) + '\n'
+        if fname in queries.keys():
+            queries[fname] += s
+        else:
+            queries[fname] = s
 
-api_link = osmapi.OsmApi()
+    elevations = []
+    for fname in queries.keys():
+        database_path = fname + '_13/w001001.adf'
 
-print('starting request')
-try:
-    map_data = api_link.Map(-83.1,33.1, -83.05,33.15)
-except: # osmapi.OsmApi.MaximumRetryLimitReachedError:  #TODO: handle errors
-    print("Could not get map data!")
+        proc = subprocess.Popen(
+            ['gdallocationinfo', database_path, '-valonly', '-geoloc'],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            universal_newlines=True
+        )
+        output, err = proc.communicate(queries[fname])
+        elevations += output.splitlines()
 
-graph = map_to_graph(map_data)
+    return elevations
 
-# follow a random path
-test_path = list()
-curnode = random.choice(tuple(graph.keys()))
-test_path.append(curnode)
-for i in range(49):
-    options = graph[curnode] - set(test_path);
-    if len(options) == 0:
-        print('no links :(')
-        break
-    curnode = random.choice(tuple(options))
-    test_path.append(curnode)
-
-
-def get_node_entry_by_id(target_node):
+def get_node_entry(target_node):
     return (item for item in map_data 
             if item["data"]["id"] == target_node).__next__()
 
-def latlong_dist(lat1_raw, lon1_raw, lat2_raw, lon2_raw):
-    lat1 = radians(float(lat1_raw))
-    lon1 = radians(float(lon1_raw))
-    lat2 = radians(float(lat2_raw))
-    lon2 = radians(float(lon2_raw))
-    # approximate radius of earth in m
-    R = 6373000.0
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = R * c
-    return distance
-
-with open('pins.txt', 'w') as trace_file:
-    for node in test_path:
-        node_info = get_node_entry_by_id(node)
-        lat = node_info['data']['lat']
-        lon = node_info['data']['lon']
-        write_string = str(lat) + ', ' + str(lon) + '\n'
-        trace_file.write(write_string)
+def get_elevations_by_nodes(nodes):
+    lats, lons = list(), list()
+    for node in nodes:
+        node_info = get_node_entry(node)
+        lats.append( float(node_info['data']['lat']) )
+        lons.append( float(node_info['data']['lon']) )
+    return get_elevations_by_coords(lats, lons)
 
 
+if __name__ == '__main__':
+    api_link = osmapi.OsmApi()
+
+    print('requesting map...')
+    try:
+        map_data = api_link.Map(-83.1,33.1, -83.05,33.15)
+    except: # osmapi.OsmApi.MaximumRetryLimitReachedError:  #TODO: handle errors
+        print("Could not get map data!")
+
+    print('converting map to graph...')
+    graph = map_to_graph(map_data)
+    print('graph completed with %d nodes' % (len(graph),))
+
+    print('finding node heights...')
+    node_heights = dict()
+    node_list = list(graph.keys())
+    elevations = get_elevations_by_nodes(node_list)
+    for i in range(len(node_list)):
+        node_heights[node_list[i]] = elevations[i]
+
+    print('identifying local maxima...')
+    local_maxima = set()
+    for node in graph.keys():
+        if node_heights[node] > max(node_heights[n] for n in graph[node]):
+            local_maxima.add(node)
+
+    print(len(local_maxima))
 
 
-'''#edge_len_max = 0
-for entry in map_data:
-    if (entry['type'] == 'node'
-            and 'data' in entry.keys()
-            and 'id' in entry['data'].keys()
-            and entry['data']['id'] in graph.keys()
-    ):
-        for neighbor in graph[entry['data']['id']]:
-            neighbor_entry = get_node_entry_by_id(neighbor)
-            
-            # distance = latlong_dist(
-            #         entry['data']['lat'],
-            #         entry['data']['lon'],
-            #         neighbor_entry['data']['lat'],
-            #         neighbor_entry['data']['lon']
-            # )
-            # print('Result:', distance)
-            # edge_len_max = max(edge_len_max, distance)
 
-#print('\n', 'Maximum:', edge_len_max)
-'''
+    '''# follow a random path
+    test_path = list()
+    curnode = random.choice(tuple(graph.keys()))
+    test_path.append(curnode)
+    for i in range(49):
+        options = graph[curnode] - set(test_path);
+        if len(options) == 0:
+            print('no links :(')
+            break
+        curnode = random.choice(tuple(options))
+        test_path.append(curnode)'''
+
+    '''def latlong_dist(lat1_raw, lon1_raw, lat2_raw, lon2_raw):
+        lat1 = radians(float(lat1_raw))
+        lon1 = radians(float(lon1_raw))
+        lat2 = radians(float(lat2_raw))
+        lon2 = radians(float(lon2_raw))
+        # approximate radius of earth in m
+        R = 6373000.0
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+        return distance'''
+
+    '''with open('pins.txt', 'w') as trace_file:
+        for node in test_path:
+            node_info = get_node_entry(node)
+            lat = node_info['data']['lat']
+            lon = node_info['data']['lon']
+            write_string = str(lat) + ', ' + str(lon) + '\n'
+            trace_file.write(write_string)'''
+
+    '''#edge_len_max = 0
+    for entry in map_data:
+        if (entry['type'] == 'node'
+                and 'data' in entry.keys()
+                and 'id' in entry['data'].keys()
+                and entry['data']['id'] in graph.keys()
+        ):
+            for neighbor in graph[entry['data']['id']]:
+                neighbor_entry = get_node_entry(neighbor)
+                
+                # distance = latlong_dist(
+                #         entry['data']['lat'],
+                #         entry['data']['lon'],
+                #         neighbor_entry['data']['lat'],
+                #         neighbor_entry['data']['lon']
+                # )
+                # print('Result:', distance)
+                # edge_len_max = max(edge_len_max, distance)
+
+    #print('\n', 'Maximum:', edge_len_max)
+    '''
