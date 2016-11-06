0a6e1dcd5c4f731c0a3bba0868b45f8e614bb251
Randal Michnovicz <randal.michnovicz@gmail.com>
2016-11-06 02:29:11 -0500

    Add tester

---
+++ acceleration_test.py
@@ -0,0 +1,15 @@
+import acceleration
+import hill_finder
+lat = 33.773963
+lng = -84.394920
+end_lng = -84.391991
+height = 275 * .0524
+v = 0
+elevation = 0
+while lng < end_lng:
+    elevation = hill_finder.get_elevations_by_coords([lat], [lng])[0]
+    lng += 1/3600/3
+    new_elevation = hill_finder.get_elevations_by_coords([lat], [lng])[0]
+    change = new_elevation - elevation
+    v = acceleration.new_velocity(v, change, 10.29)
+    print(v)
