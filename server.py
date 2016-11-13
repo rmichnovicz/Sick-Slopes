bbed2d3863c5f503be402ff412939fedfc3b62a9
Randal Michnovicz <randal.michnovicz@gmail.com>
2016-11-12 20:26:27 -0500

    Make server handle POST data in new process

--- server.py
+++ server.py
@@ -2,20 +2,30 @@
 # and render_template, to render our templates (form and response)
 # we'll use url_for to get some URLs for the app on the templates
 from flask import Flask, render_template, request, url_for
+import json
+from multiprocessing import Process
 app = Flask(__name__)
+# from hill_finder import find_hills
+# dummy method: TODO remove
+def find_hills(west, south, east, north):
+    return True
 
 # Define a route for the default URL, which loads the form
 @app.route('/', methods=['GET'])
-def show_map():
+def show_home():
     return render_template('index.htm')
 
-# Define a route for the action of the form, for example '/hello/'
-# We are also defining which type of requests this route is
-# accepting: POST requests in this case
 @app.route('/send_square/', methods=['POST'])
 def respond():
-    box = request.get_json()
+    data = request.get_json(force=True)
+    hill_finder = Process(target=find_hills, args=(
+        data['west'], data['south'], data['east'], data['north']
+    ))
+    hill_finder.daemon = True
+    hill_finder.start()
     return str(request.is_json)
+
+
 
 # Run the app :)
 if __name__ == '__main__':
