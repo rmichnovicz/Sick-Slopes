e0585e8b49db7d858313aa910d41e849af19b36c
Randal Michnovicz <randal.michnovicz@gmail.com>
2016-11-13 19:22:41 -0500

    Fix line of jQuery

---
+++ server/server.py
@@ -0,0 +1,47 @@
+# We need to import request to access the details of the POST request
+# and render_template, to render our templates (form and response)
+# we'll use url_for to get some URLs for the app on the templates
+from flask import Flask, render_template, request, url_for
+import json
+from multiprocessing import Process
+app = Flask(__name__)
+# from hill_finder import find_hills
+# dummy method: TODO remove
+def find_hills(west, south, east, north):
+    return True
+
+def is_legal_bounds(west, south, east, north):
+    return True
+
+# Define a route for the default URL, which loads the form
+@app.route('/', methods=['GET'])
+def show_home():
+    return render_template('index.htm')
+
+@app.route('/send_square/', methods=['POST'])
+def respond():
+    try:
+        data = request.get_json(force=True)
+        if not is_legal_bounds(
+            data['west'], data['south'], data['east'], data['north']
+        ):
+            return str(False)
+        hill_finder = Process(target=find_hills, args=(
+            data['west'], data['south'], data['east'], data['north']
+        ))
+        hill_finder.daemon = True
+        hill_finder.start()
+    except:
+        return str(False)
+    return str(request.is_json)
+
+
+
+# Run the app :)
+if __name__ == '__main__':
+  app.run(
+        host="0.0.0.0",
+        port=int("80")
+  )
+
+
